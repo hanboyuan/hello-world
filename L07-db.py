db7b8854900edc891f5acc1adfadf358bff566c8
hanboyuan <405186254@qq.com>
2016-01-13 17:05:24 +0800

    L07-db.py
    L07-db.py

---
+++ L07-db.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding=utf-8 -*-
+#######################
+db_picture = {
+	"p2178750670.jpg":"http://img3.douban.com/view/photo/albumcover/public/p2178750670.jpg",
+	"p2208379059.jpg":"http://img3.doubanio.com/view/photo/photo/public/p2208379059.jpg",
+	"p2214182883.jpg":"http://img3.douban.com/view/photo/photo/public/p2214182883.jpg",
+	"p17190730.jpg":"http://img3.douban.com/view/commodity_story/imedium/public/p17190730.jpg",
+	"p17065190.jpg":"http://img3.douban.com/view/commodity_story/imedium/public/p17065190.jpg",
+	"p200812139.jpg":"http://img3.doubanio.com/view/commodity_review/large/public/p200812139.jpg"
+}
