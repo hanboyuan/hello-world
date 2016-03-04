fb494445a129140af7d8e1b4b0b04277a1944d20
hanboyuan <405186254@qq.com>
2016-03-04 15:09:55 +0800

    important.py
    重要铭感信息配置文件写在里面

---
+++ important.py
@@ -0,0 +1,6 @@
+#-*- coding=utf-8 -*-
+###############################################
+conference_host = "localhost"
+conference_username = "xxxxxxxx"
+conference_password = "123456"
+conference_dbname = 'conference'
