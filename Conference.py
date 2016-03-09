ae59eac0852b79aeaae017ecba9ba06c2a0f3ee4
hanboyuan <405186254@qq.com>
2016-03-09 16:27:29 +0800

    Update Conference.py
    1.生成2个会议实例，每个实例传进去的都是验证过后的用户名
    2.a实例进行发布会议，指定主题为Python-flask2，时间为2016-3-10 16:00，会议人数为30
    3.c实例进行加入会议Python-flask2,加入成功返回True和当前预定人数，加入失败返回Flase

--- Conference.py
+++ Conference.py
@@ -28,18 +28,21 @@ class Conference(object):
         self.db.echo = False  # 如果此处设置为True的话，就会把创建数据库的sql语句显示回来
         self.meta = MetaData(self.db)
 
-
     def __str__(self):
         return "%s" % (self.__class__.__name__)
 
     __repr__ = __str__
 
+    def __session(self):
+        conferences_table = Table('conferences', self.meta, autoload=True)
+        Mapper(conference, conferences_table)
+        Session = sessionmaker(bind=self.db)
+        session = Session()
+        return session
+
     def publish(self, conference_item, conference_date, conference_person_num):
         try:
-            conferences_table = Table('conferences', self.meta, autoload=True)
-            Mapper(conference, conferences_table)
-            Session = sessionmaker(bind=self.db)
-            session = Session()
+            session = self.__session()
             tmp = session.query(conference).filter(conference.conference_item == conference_item)
             if tmp.first():
                 return False
@@ -47,7 +50,7 @@ class Conference(object):
                 conference_tmp = conference(conference_item=conference_item, conference_date=conference_date,
                                             conference_person_num=conference_person_num,
                                             conference_added_person='%s' % self.name)
-                session.add(conference_tmp)
+                session.add(conference_tmp)  # 数据的插入
                 session.commit()
                 return True
 
@@ -57,11 +60,8 @@ class Conference(object):
 
     def add_conference(self, conference_item):
         try:
-            conferences_table = Table('conferences', self.meta, autoload=True)
-            Mapper(conference, conferences_table)
-            Session = sessionmaker(bind=self.db)
-            session = Session()
-            tmp = session.query(conference).filter(conference.conference_item == conference_item)
+            session = self.__session()
+            tmp = session.query(conference).filter(conference.conference_item == conference_item)  # 数据的查
             result = tmp.first()
             if result:
                 current_add = result.conference_added_person.split()
@@ -69,9 +69,9 @@ class Conference(object):
                     current_add.append(self.name)
                     current_add_str = " ".join(current_add)
                     session.query(conference).filter(conference.conference_item == conference_item).update(
-                            {'conference_added_person': current_add_str})
+                            {'conference_added_person': current_add_str})  # 数据的改
                     session.commit()
-                    return True,current_add_str
+                    return True, current_add_str
                 else:
                     return False
             else:
@@ -94,6 +94,5 @@ if __name__ == "__main__":
     # # print a.add_conference('Python-flask')
     # print b.add_conference('Python-flask')
     # print b.add_conference('Python核心编程')
-    #print c.publish('Python-flask2', '2016-3-10 16:00', 30)
+    #print c.publish('Python-flaskxxxx', '2016-3-10 16:00', 30)
     print c.add_conference('Python-flask2')
-
